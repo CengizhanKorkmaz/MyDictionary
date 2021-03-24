cfe2d0c9187875eb4a6081310dc824fe9dd4d788
Cengizhan Korkmaz <cengizhann55@gmail.com>
2021-03-24 14:49:59 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values= new Value[0];
+
+        }
+        public void Add(Key _key,Value _value)
+        {
+            Key[] tempKey = keys;
+            Value[] tempValue = values;
+
+            keys = new Key[tempKey.Length + 1] ;
+            values = new Value[tempValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            for (int x = 0; x < tempValue.Length; x++)
+            {
+                values[x] = tempValue[x];
+            }
+            keys[keys.Length - 1] = _key;
+            values[values.Length - 1] = _value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public void ShowMe()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: " + keys[i] + " value : " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1,"Cengizhan");
+            myDictionary.Add(2, "Tuncay");
+            myDictionary.Add(3, "Miraç");
+
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.ShowMe();
+        }
+    }
+}
